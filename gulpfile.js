f74d2e457ec8233ea6ee9a76f95c0e9d18295155
Rafał Kozłowski <rafalkozlowski@MBP-15-Subeli.home>
2017-02-02 10:18:17 +0100

    zmienione pod subcms

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var nunjucksRender = require('gulp-nunjucks-render');
+var njk = require('gulp-nunjucks-render');
 var plumber = require('gulp-plumber');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
@@ -12,23 +12,33 @@ var concat = require('gulp-concat');
 // Static Server + watching sass/html files
 gulp.task('browserSync', function() {
     browserSync.init({
-        server: "dev"
+        // notify: false,
+        server: "subcms/templates"
     });
 });
 
+// setTimeout(function(){
+//   browserSync.init({
+//     notify: false,
+//     proxy: "127.0.0.1:8000",
+//     reloadDelay: 300,
+//     reloadDebounce: 500
+//   });
+// }, 2000);
+
 // nunjucks html render
-gulp.task('nunjucks', function() {
-    return gulp.src('dev/nunjucks/pages/*.nunjucks')
+gulp.task('njk', function() {
+    return gulp.src('media/njk/pages/*.njk')
         .pipe(plumber())
-        .pipe(nunjucksRender({
-            path: ['dev/nunjucks/templates/'] // String or Array
+        .pipe(njk({
+            path: ['media/njk/templates/'] // String or Array
         }))
-        .pipe(gulp.dest('dev'));
+        .pipe(gulp.dest('subcms/templates'));
 });
 
 // sass compiler
 gulp.task('sass', function() {
-    return gulp.src('dev/sass/main.sass')
+    return gulp.src('media/sass/main.sass')
         .pipe(sass({
             style: 'compressed'
         }).on('error', sass.logError))
@@ -37,124 +47,19 @@ gulp.task('sass', function() {
             cascade: false
         }))
         // .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('dev/css'))
+        .pipe(gulp.dest('media/css'))
         .pipe(browserSync.reload({
             stream: true
         }));
 });
 
-// Minify compiled CSS
-// gulp.task('minify-css', ['sass'], function() {
-//     return gulp.src('dev/css/main.css')
-//         .pipe(cleanCSS({
-//             compatibility: 'ie8'
-//         }))
-//         .pipe(rename({
-//             suffix: '.min'
-//         }))
-//         .pipe(gulp.dest('dev/css'));
-// });
-
-// Minify JS
-// gulp.task('minify-js', function() {
-//     return gulp.src('dev/js/main.js')
-//         .pipe(uglify())
-//         .pipe(rename({
-//             suffix: '.min'
-//         }))
-//         .pipe(gulp.dest('dev/js'));
-// });
-
-gulp.task('concat-js', function() {
-    return gulp.src(['dev/js/jquery.js',
-            'dev/js/bootstrap.js',
-            'dev/js/contact_me.js',
-            'dev/js/agency.js',
-            '!**/*.min.js'
-        ])
-        .pipe(concat('main.js'))
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('./dist/js'));
-});
-
-gulp.task('concat-css', function() {
-    return gulp.src(['dev/css/*.css',
-            '!**/*.min.css'
-        ])
-        .pipe(concat('main.css'))
-        .pipe(cleanCSS({
-            compatibility: 'ie8'
-        }))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('./dist/css'));
-});
-
-// copy components <-- bedzie do zmiany
-gulp.task('copy', function() {
-    // Bootstrap
-    gulp.src(['vendor/bootstrap/dist/**/*.css',
-            '!**/bootstrap-theme.*',
-            '!**/*.map'
-        ])
-        .pipe(gulp.dest('dev/'));
-    gulp.src(['vendor/bootstrap/dist/**/*.js',
-            '!**/npm.js'
-        ])
-        .pipe(gulp.dest('dev/'));
-    gulp.src(['vendor/bootstrap/dist/fonts/**', ])
-        .pipe(gulp.dest('dev/fonts'));
-    // Jquery
-    gulp.src(['vendor/jquery/dist/jquery.js',
-            'vendor/jquery/dist/jquery.min.js'
-        ])
-        .pipe(gulp.dest('dev/js'));
-    // Font-awesome
-    gulp.src([
-            'vendor/font-awesome/**/*.css',
-            '!**/*.map'
-        ])
-        .pipe(gulp.dest('dev/'));
-    gulp.src([
-            'vendor/font-awesome/fonts/**',
-        ])
-        .pipe(gulp.dest('dev/fonts'));
-});
-
-gulp.task('movecss', function() {
-    gulp.src(['vendor/**/*.min.css',
-            '!**/bootstrap-theme.*',
-            '!**/*.map'
-        ])
-        .pipe(rename({
-            dirname: ''
-        }))
-        .pipe(gulp.dest('move/css'));
-});
-
-gulp.task('movejs', function() {
-    gulp.src(['vendor/**/*.min.js',
-            '!**/npm.js',
-            '!**/*.slim.min.js',
-            '!**/sizzle.*'
-        ])
-        .pipe(rename({
-            dirname: ''
-        }))
-        .pipe(gulp.dest('move/js'));
-});
-
-gulp.task('dev', ['browserSync', 'nunjucks', 'sass'], function() {
+gulp.task('dev', ['browserSync', 'njk', 'sass'], function() {
     // Watch for changes in sass, nunjucks
-    gulp.watch("dev/sass/*.sass", ['sass']);
-    gulp.watch("dev/nunjucks/**/*.nunjucks", ['nunjucks']);
+    gulp.watch("media/sass/*.sass", ['sass']);
+    gulp.watch("media/njk/**/*.njk", ['njk']);
     // Watch for changes in html, js
-    gulp.watch("dev/*.html").on('change', browserSync.reload);
-    gulp.watch("dev/js/*.js").on('change', browserSync.reload);
+    gulp.watch("subcms/**/*.html").on('change', browserSync.reload);
+    gulp.watch("media/js/*.js").on('change', browserSync.reload);
 });
 
 gulp.task('default', function() {
