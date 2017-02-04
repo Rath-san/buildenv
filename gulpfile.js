373658a1a5a5e32b2e9a7604d6053166e667b6c2
Rafał Kozłowski <rafalkozlowski@MacBook-Pro-15-Subeli.local>
2017-02-04 11:13:33 +0100

    zminay w gulpie cd

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var njk = require('gulp-nunjucks-render');
@@ -8,8 +9,17 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var cleanCSS = require('gulp-clean-css');
 var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
+var exec = require('child_process').exec;
+var child = require('child_process');
 
-// Static Server + watching sass/html files
+gulp.task('img', function() {
+    gulp.src('media/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('media/images'));
+});
+
+// Static Server
 gulp.task('browserSync', function() {
     browserSync.init({
         // notify: false,
@@ -19,15 +29,6 @@ gulp.task('browserSync', function() {
         }
     );
 });
-
-// setTimeout(function(){
-//   browserSync.init({
-//     notify: false,
-//     proxy: "127.0.0.1:8000",
-//     reloadDelay: 300,
-//     reloadDebounce: 500
-//   });
-// }, 2000);
 
 // nunjucks html render
 gulp.task('njk', function() {
@@ -56,7 +57,7 @@ gulp.task('sass', function() {
         }));
 });
 
-gulp.task('dev', ['browserSync', 'njk', 'sass'], function() {
+gulp.task('front', ['browserSync', 'njk', 'sass'], function() {
     // Watch for changes in sass, nunjucks
     gulp.watch("media/sass/*.sass", ['sass']);
     gulp.watch("media/njk/**/*.njk", ['njk']);
@@ -67,4 +68,45 @@ gulp.task('dev', ['browserSync', 'njk', 'sass'], function() {
 
 gulp.task('default', function() {
     console.log('hello world!, soon to be dist compiler');
+});
+
+// BACKEND ===============================================
+gulp.task('django', function(){
+
+  var spawn = child.spawn;
+  console.info('Starting django server');
+  var PIPE = {
+    stdio: 'inherit'
+  };
+  spawn('python', ['manage.py','runserver'], PIPE);
+});
+
+gulp.task('runserver', function() {
+    var isWin = /^win/.test(process.platform);
+     var cmd =  'source .../venv/bin/activate';
+     console.log(cmd);
+    if (isWin) { //for Windows
+        cmd = '..\\Scripts\\activate';
+    }
+
+    var proc = exec(cmd);
+});
+
+gulp.task('default', ['django','sass'], function() {
+  console.log('hello world!, watch, sass, js');
+  setTimeout(function(){
+    browserSync.init({
+      notify: false,
+      proxy: "127.0.0.1:8000",
+      reloadDelay: 300,
+      reloadDebounce: 500
+    });
+  }, 2000);
+  // Watch for changes in sass, nunjucks
+  gulp.watch("media/sass/*.sass", ['sass']);
+  // gulp.watch("nunjucks/**/*.njk", ['nunjucks']);
+  // Watch for changes in html, js
+  gulp.watch("templates/**/*.html").on('change', reload);
+  gulp.watch("media/js/*.js").on('change', reload);
+  // gulp.watch(["../subcms/**/*.html"], reload);
 });
