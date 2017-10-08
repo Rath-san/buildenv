87e8662e016eb38b11667ed1337e965ba2d68df0
Rath-san <rim.kozlowski@gmail.com>
2017-10-08 09:36:22 +0200

    to pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,75 @@
 var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var reload = browserSync.reload;
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var njk = require('gulp-nunjucks-render');
 var plumber = require('gulp-plumber');
 var rename = require('gulp-rename');
-var imagemin = require('gulp-imagemin');
-var exec = require('child_process').exec;
-var child = require('child_process');
 
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+
+var pug = require('gulp-pug');
+var data = require('gulp-data');
+
+// TODO: implement imagemin @1
+// var imagemin = require('gulp-imagemin');
+
+// var exec = require('child_process').exec;
+// var child = require('child_process');
+// var path = require('path');
+
+
+// TODO: @1
 // gulp.task('img', function() {
 //     gulp.src('media/images/**/*')
 //         .pipe(imagemin())
 //         .pipe(gulp.dest('media/images'));
 // });
 
-// Static Server
-gulp.task('browserSync', function() {
-    browserSync.init({
-        // notify: false,
-        server: {
-          baseDir: ["./", "subcms/templates"]
-          }
-        }
-    );
-});
 
-// gulp.task('views', function buildHTML() {
-//   return gulp.src('views/*.pug')
-//   .pipe(pug({
-//     // Your options in here.
-//   }))
+// Static Server
+// gulp.task('browserSync', function() {
+//     browserSync.init({
+//         // notify: false,
+//         server: {
+//           baseDir: [".", "_html"]
+//           }
+//         }
+//     );
 // });
 
-// nunjucks html render
-gulp.task('njk', function() {
-    return gulp.src('media/njk/pages/*.njk')
-        .pipe(plumber())
-        .pipe(njk({
-            path: ['media/njk/templates/'] // String or Array
-        }))
-        .pipe(gulp.dest('subcms/templates'));
+// .pug with json parser
+gulp.task('pug', function buildHTML() {
+  return gulp.src('_pug/pages/*.pug')
+  .pipe(data(function() {
+    return require('./db.json');
+  }))
+  .pipe(plumber())
+  .pipe(pug({
+    pretty: true
+    // Your options in here.
+  }))
+  .pipe(gulp.dest('_html'))
+  .pipe(browserSync.reload({
+      stream: true
+  }));
 });
 
-
+// sass compiler
+var bowerPath = 'bower_components'
 var sassPaths = [
-  'bower_components/bootstrap-sass/assets/stylesheets'
+  bowerPath + '/bootstrap-sass/assets/stylesheets',
+  bowerPath + '/font-awesome/scss/',
+  bowerPath + '/material-design-lite/src/'
 ];
 
-
-// sass compiler
 gulp.task('sass', function() {
-    return gulp.src(['media/sass/*.sass','media/sass/*.scss'])
+    return gulp.src(['media/sass/*.{sass, scss}'])
+        .pipe(sourcemaps.init())
         .pipe(sass({
             includePaths: sassPaths,
-            // outputStyle: 'compressed'
+            outputStyle: ''
         }).on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: [
@@ -70,58 +84,28 @@ gulp.task('sass', function() {
             ],
             cascade: false
         }))
-        // .pipe(header(banner, { pkg: pkg }))
+        .pipe(sourcemaps.write(''))
         .pipe(gulp.dest('media/css'))
         .pipe(browserSync.reload({
             stream: true
         }));
 });
 
-gulp.task('front', ['browserSync', 'njk', 'sass'], function() {
+
+var log = function(){
+  console.log("1");
+}
+
+
+// watch
+gulp.task('front', ['pug', 'sass'], function() {
+    browserSync.init({
+        // notify: false,
+      server: {
+        baseDir: ["./", "_html"]
+      }
+    });
+    gulp.watch("_pug/**/*.pug", ['pug']);
     gulp.watch("media/sass/**/*.{sass,scss}", ['sass']);
     gulp.watch("media/js/*.js").on('change', reload);
-    gulp.watch("media/njk/**/*.njk", ['njk']);
-    gulp.watch("subcms/**/*.html").on('change', reload);
-
 });
-
-// BACKEND ===============================================
-// gulp.task('django', function(){
-//
-//   var spawn = child.spawn;
-//   console.info('Starting django server');
-//   var PIPE = {
-//     stdio: 'inherit'
-//   };
-//   spawn('python', ['manage.py','runserver'], PIPE);
-// });
-//
-// gulp.task('runserver', function() {
-//     var isWin = /^win/.test(process.platform);
-//      var cmd =  'source .../venv/bin/activate';
-//      console.log(cmd);
-//     if (isWin) { //for Windows
-//         cmd = '..\\Scripts\\activate';
-//     }
-//
-//     var proc = exec(cmd);
-// });
-//
-// gulp.task('default', ['django','sass'], function() {
-//   console.log('hello world!, watch, sass, js');
-//   setTimeout(function(){
-//     browserSync.init({
-//       notify: true,
-//       proxy: "127.0.0.1:8000",
-//       reloadDelay: 300,
-//       reloadDebounce: 500
-//     });
-//   }, 2000);
-//   // Watch for changes in sass, nunjucks
-//   gulp.watch("media/sass/**/*.+(sass,scss)", ['sass']);
-//   // gulp.watch("nunjucks/**/*.njk", ['nunjucks']);
-//   // Watch for changes in html, js
-//   gulp.watch("templates/**/*.html").on('change', reload);
-//   gulp.watch("media/js/*.js").on('change', reload);
-//   // gulp.watch(["../subcms/**/*.html"], reload);
-// });
